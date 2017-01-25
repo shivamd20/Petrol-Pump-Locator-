904d042e69ba53025bedcd8a3d8d9f1b06e87ec9
shivamd20 <shivamd20@gmail.com>
2017-01-25 22:12:20 +0530

    Initial commit

---
+++ app/src/main/java/shivam_a/prayas/MainActivity.java
@@ -0,0 +1,13 @@
+package shivam_a.prayas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
